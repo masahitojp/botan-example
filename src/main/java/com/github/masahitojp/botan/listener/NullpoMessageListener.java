91de9da4934737e98948c515f31c4701c83f3bdb
masahitojp <randomstep@gmail.com>
2015-08-26 01:00:58 +0900

    add nullpo handler

---
+++ src/main/java/com/github/masahitojp/botan/listener/NullpoMessageListener.java
@@ -0,0 +1,30 @@
+package com.github.masahitojp.botan.listener;
+
+import com.github.masahitojp.botan.exception.BotanException;
+import com.github.masahitojp.botan.utils.BotanUtils;
+
+@SuppressWarnings("unused")
+public class NullpoMessageListener implements BotanMessageListenerRegister {
+
+    @Override
+    public void register() {
+        BotanUtils.respond(
+                "ぬるぽ|ヌルポ|nullpo",
+                "rich: Use rich style ｶﾞｯ",
+                message -> {
+                    try {
+final String ga =
+        "　 Λ＿Λ　　　　＼＼\n" +
+        " （　・∀・）　　　|　|　ｶﾞｯ\n" +
+        "と　　　　）　　　|　|\n" +
+        "　 Ｙ　/ノ　　　 人\n" +
+        "　　 /　）　 　 < 　>_Λ∩\n" +
+        " ＿/し´　／／. Ｖ｀Д´）/\n" +
+        "（＿フ彡　　　　　　 /　>>@%s";
+                        message.reply(String.format(ga, message.getFrom()));
+                    } catch (BotanException e) {
+                        e.printStackTrace();
+                    }
+                });
+    }
+}
