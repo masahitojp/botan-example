3a1e9ef661b364a3e4a408df110991f7ef7b380c
masahitojp <randomstep@gmail.com>
2015-09-15 01:22:18 +0900

    modify botan-core 0.0.0.25

--- src/main/java/com/github/masahitojp/botan/SlackBot.java
+++ src/main/java/com/github/masahitojp/botan/SlackBot.java
@@ -1,8 +1,6 @@
 package com.github.masahitojp.botan;
 
-import com.github.masahitojp.botan.adapter.ComandLineAdapter;
 import com.github.masahitojp.botan.adapter.SlackAdapter;
-import com.github.masahitojp.botan.brain.MapDBBrain;
 import com.github.masahitojp.botan.brain.RedisBrain;
 import com.github.masahitojp.botan.exception.BotanException;
 import com.github.masahitojp.botan.utils.BotanUtils;
@@ -10,17 +8,22 @@ import com.github.masahitojp.botan.utils.BotanUtils;
 public class SlackBot {
 
 
-	static public void main(String[] Args) {
+    static public void main(String[] Args) {
 
         final String team = BotanUtils.envToOpt("SLACK_TEAM").orElse("");
         final String user = BotanUtils.envToOpt("SLACK_USERNAME").orElse("");
         final String pswd = BotanUtils.envToOpt("SLACK_PASSWORD").orElse("");
         final String channel = BotanUtils.envToOpt("SLACK_ROOM").orElse("");
-        final String mapDBPath = BotanUtils.envToOpt("MAPDB_PATH").orElse("botanDB");
-        final String mapDBName = BotanUtils.envToOpt("MAPDB_NAME").orElse(team);
-
-        final Botan botan = new Botan.BotanBuilder(new ComandLineAdapter())
-                .setBrain(new RedisBrain("localhost", 6379))
+        final String redisHost = BotanUtils.envToOpt("REDIS_HOST").orElse("localhost");
+        final String redisPort = BotanUtils.envToOpt("REDIS_PORT").orElse("");
+        int port;
+        try {
+            port = Integer.parseInt(redisPort);
+        } catch (final NumberFormatException ex) {
+            port = 6379;
+        }
+        final Botan botan = new Botan.BotanBuilder(new SlackAdapter(team, user, pswd, channel))
+                .setBrain(new RedisBrain(redisHost, port))
                 .build();
 
         try {
@@ -29,5 +32,5 @@ public class SlackBot {
             ex.printStackTrace();
         }
 
-	}
+    }
 }
