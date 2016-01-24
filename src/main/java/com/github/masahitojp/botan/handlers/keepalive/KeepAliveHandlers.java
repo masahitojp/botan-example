59a3b01edf0ff9e9ac5a02fe4d646e1a4f589088
masahitojp <randomstep@gmail.com>
2016-01-25 02:17:36 +0900

    version up botan-core

--- src/main/java/com/github/masahitojp/botan/handlers/keepalive/KeepAliveHandlers.java
+++ src/main/java/com/github/masahitojp/botan/handlers/keepalive/KeepAliveHandlers.java
@@ -5,36 +5,13 @@ import com.github.masahitojp.botan.handler.BotanMessageHandlers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.util.Optional;
 
 @SuppressWarnings("unused")
 public class KeepAliveHandlers implements BotanMessageHandlers {
     private static Logger logger = LoggerFactory.getLogger(KeepAliveHandlers.class);
-    // TODO add feature: robot.route.get / post method
-    private App app;
-
-    @Override
-    public final void initialize(final Robot robot) {
-        try {
-            // IP設定
-            final String ipAddress = Optional.ofNullable(System.getenv("IP_ADDRESS")).orElse("0.0.0.0");
-            final int port = Integer.parseInt(Optional.ofNullable(System.getenv("PORT")).orElse("5353"));
-            app = new App(ipAddress, port);
-            app.start();
-        } catch (IOException e) {
-            logger.warn("", e);
-        }
-    }
 
     @Override
     public final void register(final Robot robot) {
-    }
-
-    @Override
-    public final void beforeShutdown() {
-        if(app != null) {
-            app.stop();
-        }
+        robot.router.GET("/botan/keepalive", (req, resp) -> 200);
     }
 }
